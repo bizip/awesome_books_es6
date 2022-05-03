7a6015ca7baa2fc6c068b786954b24921cf106af
bizip <bizip04@gmail.com>
2022-05-03 17:49:21 +0200

    Feature add a tme and date using luxon.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { DateTime } from './modules/luxon.js';
 import Book from './modules/books.js';
 import { contactPage, booksList, addBook } from './modules/toggleDisplay.js';
 
@@ -9,6 +10,7 @@ formData.addEventListener('submit', (e) => {
   const getTitle = document.getElementById('title').value;
   const getAuthor = document.getElementById('author').value;
   booksLibrary.addNewBook(getTitle, getAuthor);
+  formData.reset();
 });
 
 const listLink = document.getElementById('list-link');
@@ -22,4 +24,8 @@ contactLink.addEventListener('click', addBook);
 window.onload = () => {
   document.querySelector('.ADD').style.display = 'none';
   document.querySelector('.contact').style.display = 'none';
+  const setDate = document.getElementById('date-time');
+  const date = new Date();
+  const timeNow = DateTime.fromJSDate(date);
+  setDate.innerHTML = timeNow.toLocaleString(DateTime.DATETIME_MED);
 };
--- modules/books.js
+++ modules/books.js
@@ -3,43 +3,43 @@ export default class Book {
     this.bookStore = JSON.parse(localStorage.getItem('singleBook')) || [];
   }
 
-  getBookList() {
-    const bookList = document.getElementById('book-list');
-    bookList.innerHTML = '';
-    bookList.innerHTML += this.bookStore.map(
-      (el, index) => ` <div class="${index % 2 === 0 ? 'dark' : 'mediumdark'}">
+    getBookList = () => {
+      const bookList = document.getElementById('book-list');
+      bookList.innerHTML = '';
+      bookList.innerHTML += this.bookStore.map(
+        (el, index) => ` <div class="${index % 2 === 0 ? 'dark' : 'mediumdark'}">
           <h4>${el.title}</h4>
           <p>${el.author}</p>
           <button type="button" id=${el.id} class='remove-button'>Remove</button>
           </div>`,
-    );
-    const Allbtn = document.querySelectorAll('.remove-button');
-    Allbtn.forEach((btn) => {
-      btn.addEventListener('click', () => {
-        this.deleteBook(btn.id);
+      );
+      const Allbtn = document.querySelectorAll('.remove-button');
+      Allbtn.forEach((btn) => {
+        btn.addEventListener('click', () => {
+          this.deleteBook(btn.id);
+        });
       });
-    });
-  }
+    }
 
-  addNewBook(title, author) {
-    const id = this.bookStore.length + 1;
-    const addbook = {
-      title,
-      author,
-      id,
-    };
-    this.bookStore.push(addbook); localStorage.setItem('singleBook', JSON.stringify(this.bookStore));
-    this.getBookList();
-  }
+    addNewBook = (title, author) => {
+      const id = this.bookStore.length + 1;
+      const addbook = {
+        title,
+        author,
+        id,
+      };
+      this.bookStore.push(addbook); localStorage.setItem('singleBook', JSON.stringify(this.bookStore));
+      this.getBookList();
+    }
 
-  deleteBook(id) {
-    this.bookStore = this.bookStore.filter((item) => {
-      if (item.id === Number(id)) {
-        return false;
-      }
-      return true;
-    });
-    localStorage.setItem('singleBook', JSON.stringify(this.bookStore));
-    this.getBookList();
-  }
+    deleteBook = (id) => {
+      this.bookStore = this.bookStore.filter((item) => {
+        if (item.id === Number(id)) {
+          return false;
+        }
+        return true;
+      });
+      localStorage.setItem('singleBook', JSON.stringify(this.bookStore));
+      this.getBookList();
+    }
 }
